596a726635e01bdc26e45a481b0b214762662de5
Saurav Pal <resyfer.dev@gmail.com>
2024-05-22 10:43:07 +0000

    fs/mnemofs: Adds mnemofs and mnemofs journal Adds basic setup for mnemofs NAND Flash file system, its journal and some required utilities.
    Signed-off-by: Saurav Pal <resyfer.dev@gmail.com>

---
+++ fs/mnemofs/mnemofs.c
@@ -0,0 +1,98 @@
+/****************************************************************************
+ * fs/mnemofs/mnemofs.c
+ * mnemofs: Filesystem for NAND Flash storage devices.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/fs/fs.h>
+
+#include "mnemofs.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Types
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Function Prototypes
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: g_mnemofs_operations
+ *
+ * Description:
+ *  The global list of VFS methods implemented by mnemofs
+ *
+ ****************************************************************************/
+
+const struct mountpt_operations g_mnemofs_operations =
+{
+  NULL, /* open */
+  NULL, /* close */
+  NULL, /* read */
+  NULL, /* write */
+  NULL, /* seek */
+  NULL, /* ioctl */
+  NULL, /* mmap */
+  NULL, /* truncate */
+  NULL, /* poll */
+
+  NULL, /* sync */
+  NULL, /* dup */
+  NULL, /* fstat */
+  NULL, /* fchstat */
+
+  NULL, /* opendir */
+  NULL, /* closedir */
+  NULL, /* readdir */
+  NULL, /* rewinddir */
+
+  NULL, /* bind */
+  NULL, /* unbind */
+  NULL, /* statfs */
+
+  NULL, /* unlink */
+  NULL, /* mkdir */
+  NULL, /* rmdir */
+  NULL, /* rename */
+  NULL, /* stat */
+  NULL  /* chstat */
+};
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
